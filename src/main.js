0a1ac4191f97dafa082e8b25af7ebde820ffae92
jiucheng <15279232381@163.com>
2019-12-27 14:39:27 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
+// 路由实际上是根据网址的不同，显示不同的内容
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
