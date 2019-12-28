eee2b2d44d9d4f348aec3256d2d25ace4513e4e4
jiucheng <15279232381@163.com>
2019-12-28 14:12:15 +0800

    add dataIndex

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="icons">
-        <swiper>
+        <swiper :options="swiperOption">
             <swiper-slide v-for="(page,index) of pages" :key="index">
                 <div class="icon" v-for="item of page" :key="item.id">
                     <div class="icon-img">
-                            <img class="icon-img-content" :src="item.img">
+                            <img class="icon-img-content" :src="item.imgUrl">
                     </div>
                     <p class="icon-desc">{{ item.desc }}</p>
                 </div>
@@ -16,66 +16,76 @@
 <script>
 export default {
   name: 'HomeIcons',
+  // data () {
+  //   return {
+  //     iconList: [
+  //       {
+  //         id: '0001',
+  //         img: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+  //         desc: '景点门票'
+  //       },
+  //       {
+  //         id: '0002',
+  //         img: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+  //         desc: '一日游'
+  //       },
+  //       {
+  //         id: '0003',
+  //         img: 'http://img1.qunarzz.com/piao/fusion/1803/de/f26be47a6bf02a02.png',
+  //         desc: '上海迪士尼'
+  //       },
+  //       {
+  //         id: '0004',
+  //         img: 'https://img1.qunarzz.com/piao/fusion/1810/50/26ffa31b56646402.png',
+  //         desc: '上海海昌'
+  //       },
+  //       {
+  //         id: '0005',
+  //         img: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201912/9480e99a101564a22dfff0c4b9d0861c.png',
+  //         desc: '上海杜莎'
+  //       },
+  //       {
+  //         id: '0006',
+  //         img: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/3ef092d0a89ffddb7f781cda30c8ae49.png',
+  //         desc: '错峰出游'
+  //       },
+  //       {
+  //         id: '0007',
+  //         img: 'http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png',
+  //         desc: '温泉滑雪'
+  //       },
+  //       {
+  //         id: '0008',
+  //         img: 'http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',
+  //         desc: '黄浦江游船'
+  //       },
+  //       {
+  //         id: '0009',
+  //         img: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201912/9480e99a101564a22dfff0c4b9d0861c.png',
+  //         desc: '上海杜莎'
+  //       },
+  //       {
+  //         id: '0010',
+  //         img: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+  //         desc: '上海野生动物园'
+  //       }
+  //     ]
+  //   }
+  // },
+  props: {
+    list: Array
+  },
   data () {
     return {
-      iconList: [
-        {
-          id: '0001',
-          img: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
-          desc: '景点门票'
-        },
-        {
-          id: '0002',
-          img: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
-          desc: '一日游'
-        },
-        {
-          id: '0003',
-          img: 'http://img1.qunarzz.com/piao/fusion/1803/de/f26be47a6bf02a02.png',
-          desc: '上海迪士尼'
-        },
-        {
-          id: '0004',
-          img: 'https://img1.qunarzz.com/piao/fusion/1810/50/26ffa31b56646402.png',
-          desc: '上海海昌'
-        },
-        {
-          id: '0005',
-          img: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201912/9480e99a101564a22dfff0c4b9d0861c.png',
-          desc: '上海杜莎'
-        },
-        {
-          id: '0006',
-          img: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/3ef092d0a89ffddb7f781cda30c8ae49.png',
-          desc: '错峰出游'
-        },
-        {
-          id: '0007',
-          img: 'http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png',
-          desc: '温泉滑雪'
-        },
-        {
-          id: '0008',
-          img: 'http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',
-          desc: '黄浦江游船'
-        },
-        {
-          id: '0009',
-          img: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201912/9480e99a101564a22dfff0c4b9d0861c.png',
-          desc: '上海杜莎'
-        },
-        {
-          id: '0010',
-          img: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
-          desc: '上海野生动物园'
-        }
-      ]
+      swiperOption: {
+        autoplay: false
+      }
     }
   },
   computed: {
     pages () {
       const pages = []
-      this.iconList.forEach((item, index) => {
+      this.list.forEach((item, index) => {
         const page = Math.floor(index / 8)
         if (!pages[page]) {
           pages[page] = []
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="wrapper">
-        <swiper :options="swiperOption">
+        <swiper :options="swiperOption" v-if="showList">
             <!-- slides -->
-            <swiper-slide v-for="item of swiperList" :key="item.id">
+            <swiper-slide v-for="item of list" :key="item.id">
                 <img  class="swiper-img" :src="item.imgUrl" />
             </swiper-slide>
             <!-- Optional controls -->
@@ -22,32 +22,40 @@ export default {
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true
-      },
-      swiperList: [
-        {
-          id: '0001',
-          imgUrl: 'https://h-des-activity-fecp.qunarzz.com/h_des_activity_fe_h_des_activity_fe/8c54576e74c6d4ea0062c9a664035262.jpg_750x200_9a39efd3.jpg'
-        },
-        {
-          id: '0002',
-          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201912/d6df0db510d7b9aaa3d9ce4cffafeca1.jpg_750x200_abb38f14.jpg'
-        },
-        {
-          id: '0003',
-          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/0645bd625b46216a55e3835d0f677ce2.jpg_750x200_1e9a0cbf.jpg'
-        },
-        {
-          id: '0004',
-          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/4d9e45775ba63057e10f3c4d9d7b8cdd.jpg_750x200_345d57f5.jpg'
-        },
-        {
-          id: '0005',
-          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20198/083c708a3949958bcbca0fcc0ea3d57f.jpg_750x200_e47acfc7.jpg'
-        },
-        {
-          id: '0006',
-          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201912/37c4edf7c033ada7b61a1f9d0acffd08.jpg_750x200_3223c943.jpg'
-        }]
+      }
+      // swiperList: [
+      //   {
+      //     id: '0001',
+      //     imgUrl: 'https://h-des-activity-fecp.qunarzz.com/h_des_activity_fe_h_des_activity_fe/8c54576e74c6d4ea0062c9a664035262.jpg_750x200_9a39efd3.jpg'
+      //   },
+      //   {
+      //     id: '0002',
+      //     imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201912/d6df0db510d7b9aaa3d9ce4cffafeca1.jpg_750x200_abb38f14.jpg'
+      //   },
+      //   {
+      //     id: '0003',
+      //     imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/0645bd625b46216a55e3835d0f677ce2.jpg_750x200_1e9a0cbf.jpg'
+      //   },
+      //   {
+      //     id: '0004',
+      //     imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/4d9e45775ba63057e10f3c4d9d7b8cdd.jpg_750x200_345d57f5.jpg'
+      //   },
+      //   {
+      //     id: '0005',
+      //     imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20198/083c708a3949958bcbca0fcc0ea3d57f.jpg_750x200_e47acfc7.jpg'
+      //   },
+      //   {
+      //     id: '0006',
+      //     imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201912/37c4edf7c033ada7b61a1f9d0acffd08.jpg_750x200_3223c943.jpg'
+      //   }]
+    }
+  },
+  props: {
+    list: Array
+  },
+  computed: {
+    showList () {
+      return this.list.length
     }
   }
 }
